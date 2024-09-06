70ac6d37e1ad5f6a3aaabd1c0b2a6896377fe71f
sauljimenez24 <a.22308051281222@cbtis128.edu.mx>
2024-09-06 09:40:57 -0600

    Add files via upload

---
+++ miscadenas.py
@@ -0,0 +1,19 @@
+nombre="saul"
+print(nombre[0])
+print(nombre)
+for c in "saul":
+  print(c)
+MisNovias=["sofia", "anania", "lesly"]
+for chicas in MisNovias:
+  print(chicas)
+  print("otro ejemplo")
+  b = "Saul JImenez"
+  print(b[2:5])
+  print (" a mayuscalas")
+  a = "saul jimenez"
+  print(a.upper())
+  print("concatenar")
+  a = "maria"
+b = "perez"
+c = a +"-"+ b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,15 @@
+import math
+edad=17 # 17 se guarda en edad
+nombre="Camila Rodriguez"
+print("tu edad es " ,edad)
+print("te llamas es " ,nombre)
+print ("-- tipo de variables ---")
+x = str(7)    # x will be '3'
+y = int(3.1415)    # y will be 3
+z=float(7)
+print(x)
+print(y)
+print(z)
+print("-- funciones matematicas ---")
+x = math.sqrt(64)
+print(x)
